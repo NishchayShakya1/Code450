ffe7790e8bae11ffa45e42b6b2167ec0e5436fa2
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2022-05-27 10:21:10 +0530

    Time: 108 ms (74.70%), Space: 46.7 MB (71.96%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int>s;
-        for(int i=0; i<nums.size(); i++){
-            s.insert(nums[i]);
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
         }
         
-        if(s.size() == nums.size()){
-            return false;
-        }
-        
-        return true;
+        return false;
     }
 };
