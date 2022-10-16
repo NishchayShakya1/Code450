328c10e5ba627005e8511e9e3bfe1ea8cd9c0d6f
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2022-10-16 15:43:33 +0530

    Time: 56 ms (20.21%), Space: 17.8 MB (93.62%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int count = 0;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(count != nums[i]){
+                break;
+            }
+            count++;
+        }
+        
+        return count;
+    }
+};
