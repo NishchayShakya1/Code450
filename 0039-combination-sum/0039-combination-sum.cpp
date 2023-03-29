07ffb95133f5987ce799b6f202f8b642bf921161
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2023-03-29 21:54:23 +0530

    Time: 7 ms (67.98%), Space: 17.1 MB (29.72%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    
+    void sol(int idx, vector<int>&candidates, vector<int>&temp, int target){
+        
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+        
+        if(target<0){
+            return;
+        }
+        
+        if(idx == candidates.size()){
+            return;
+        }
+        
+        sol(idx+1, candidates, temp, target);
+        
+        temp.push_back(candidates[idx]);
+        sol(idx, candidates, temp, target-candidates[idx]);
+        temp.pop_back();
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        sol(0, candidates, temp, target);
+        return ans;
+    }
+};
