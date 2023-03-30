d0b3d2eb3ef9baeaa4ac8e39e0efc17f292c1ec5
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2023-03-30 10:11:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.60%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -3,8 +3,8 @@ public:
     int titleToNumber(string columnTitle) {
         int result = 0;
         for(auto c: columnTitle){
-            int d = c - 'A' + 1;
-            result = result*26 + d;
+            int d = c - 'A';
+            result = result*26 + d +1;
         }
         
         return result;
