49736ab2bad592535f0bc4299818f2745a9ac904
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2023-04-08 10:52:49 +0530

    Time: 12 ms (89.30%), Space: 10.9 MB (80.77%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string a;
+        for (auto& c : s) 
+            if (a.size() && a.back() == c) a.pop_back();
+            else a.push_back(c);
+        return a;
+    }
+};
