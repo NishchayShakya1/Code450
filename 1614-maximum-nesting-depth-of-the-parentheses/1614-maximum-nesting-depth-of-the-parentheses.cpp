9231567f0069fd3cdbc91acfa7e6b3c863089e8f
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2023-04-14 20:23:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (64.49%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int res = 0;
+        int depth = 0;
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '('){
+                depth++;
+            }
+            else if(s[i] == ')'){
+                --depth;
+            }
+            
+            res = max(res, depth);
+        }
+        
+        return res;
+    }
+};
