f18ee156e2e0490ab870e4cb7416c5ffb9db6741
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2023-04-25 11:51:33 +0530

    Time: 130 ms (28.40%), Space: 46.2 MB (25.04%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,23 +1,40 @@
 class SmallestInfiniteSet {
 public:
     
-   set<int>st;
+    set<int>ans;
     SmallestInfiniteSet() {
-        st.clear();
-        for(int i=1;i<=1000;i++){
-            st.insert(i);
+        ans.clear();
+        for(int i=1; i<=1000; i++){
+            ans.insert(i);
         }
     }
     
     int popSmallest() {
-        int t = *st.begin();
-        st.erase(st.begin());
-        return t;
+        int res = *ans.begin();
+        ans.erase(ans.begin());
+        return res;
     }
     
     void addBack(int num) {
-        st.insert(num);
+        ans.insert(num);
     }
+//      set<int>st;
+//     SmallestInfiniteSet() {
+//         st.clear();
+//         for(int i=1;i<=1000;i++){
+//             st.insert(i);
+//         }
+//     }
+    
+//     int popSmallest() {
+//         int t = *st.begin();
+//         st.erase(st.begin());
+//         return t;
+//     }
+    
+//     void addBack(int num) {
+//         st.insert(num);
+//     }
 };
 
 /**
