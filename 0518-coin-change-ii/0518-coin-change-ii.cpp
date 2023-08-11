4d453236e95ea363e49cb47e4ee2d3af84bdbeb1
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2023-08-11 10:50:12 +0530

    Time: 8 ms (92.00%), Space: 6.9 MB (99.07%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int dp[amount+1];
+        for(int i=0; i<amount+1; i++){
+            dp[i] = 0;
+        }
+        dp[0] = 1;
+        for(int i=0; i<coins.size(); i++){
+            for(int j=coins[i]; j<=amount; j++){
+                dp[j]+=dp[j-coins[i]];
+            }
+        }
+        
+        return dp[amount];
+    }
+};
